68b5ac1e02e403ca897672ed941111e7d8d44385
paulorinze <paulo.rinze@uzambeze.onmicrosoft.com>
2023-06-05 14:06:25 +0100

    Updates

---
+++ precache-manifest.55f33b174784741c4c1c279b5ea6a40a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3be1e604672d9a3ab738",
+    "url": "/robofriends/static/css/main.5df4878b.chunk.css"
+  },
+  {
+    "revision": "3be1e604672d9a3ab738",
+    "url": "/robofriends/static/js/main.641a0e83.chunk.js"
+  },
+  {
+    "revision": "cb45653fdb1cd181cc7c",
+    "url": "/robofriends/static/js/runtime~main.465d8a73.js"
+  },
+  {
+    "revision": "fc8825ad7f9c36fb2667",
+    "url": "/robofriends/static/css/2.4438bbb0.chunk.css"
+  },
+  {
+    "revision": "fc8825ad7f9c36fb2667",
+    "url": "/robofriends/static/js/2.39830d21.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/robofriends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4c2d51d5f01d65f5fc0ec234f686af46",
+    "url": "/robofriends/index.html"
+  }
+];
